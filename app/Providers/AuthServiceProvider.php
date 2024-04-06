c4c583470a1d557d132351159e54c3c7c1db8f79
MarinKawashima <marin.kthv@gmail.com>
2024-04-06 16:26:06 +0900

    AuthServiceProviderディレクトリを変更

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -35,12 +35,12 @@ class AuthServiceProvider extends ServiceProvider
 
             return (new MailMessage)
 
-            // ↓↓ここを編集する！！Mailモデルから代入する？
-                ->subject($mailMessage->subject ?? 'Verify Email Address')//←←件名（ex.メールアドレス確認）
-                ->line($mailMessage->line1 ?? 'Click the button below to verify your email address.')//←本文（ex.下のボタンをクリックしてメールアドレスを確認してください）
-                ->action($mailMessage->action ?? 'Verify Email Address', $url)//←メールの確認リンク（ボタンに表示される文言　ex.メールアドレスの確認）
-                ->line($mailMessage->line2 ?? 'Thank you for using our application!')//(私達のアプリケーションをご利用いただき、ありがとうございます。)
-                ->line($mailMessage->line3 ?? 'If you did not create an account, no further action is required.');//'心当たりがない場合は、本メールを破棄してください。よろしくお願いいたします。NAGOYAMESHI'
+        
+            ->subject($mailMessage->subject ?? 'Verify Email Address')//←←件名（ex.メールアドレス確認）
+            ->line($mailMessage->line1 ?? 'Click the button below to verify your email address.')//←本文（ex.下のボタンをクリックしてメールアドレスを確認してください）
+            ->action($mailMessage->action ?? 'Verify Email Address', $url)//←メールの確認リンク（ボタンに表示される文言　ex.メールアドレスの確認）
+            ->line($mailMessage->line2 ?? 'Thank you for using our application!')//(私達のアプリケーションをご利用いただき、ありがとうございます。)
+            ->line($mailMessage->line3 ?? 'If you did not create an account, no further action is required.');//'心当たりがない場合は、本メールを破棄してください。よろしくお願いいたします。NAGOYAMESHI'
         });
     }
 }
